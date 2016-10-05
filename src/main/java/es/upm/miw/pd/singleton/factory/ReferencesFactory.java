74184059ca62d881ce5ebaa361e53578099a1b8b
Jahir <Jahir@LAPTOP-07M3ALHS>
2016-10-05 20:31:59 +0200

    Solución Singleton-Factory

--- src/main/java/es/upm/miw/pd/singleton/factory/ReferencesFactory.java
+++ src/main/java/es/upm/miw/pd/singleton/factory/ReferencesFactory.java
@@ -4,27 +4,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ReferencesFactory {
-    private Map<String, Integer> references;
+	private Map<String, Integer> references;
 
-    private int reference;
+	private static ReferencesFactory instanceReferencesFactory;
 
-    public ReferencesFactory() {
-        this.references = new HashMap<>();
-        this.reference = 0;
-    }
+	private int reference;
 
-    public int getReference(String key) {
-        Integer result = this.references.get(key);
-        if (result == null) {
-            this.references.put(key, this.reference);
-            result = this.reference;
-            reference++;
-        }
-        return result;
-    }
+	private ReferencesFactory() {
+		this.references = new HashMap<>();
+		this.reference = 0;
+	}
 
-    public void removeReference(String key) {
-        this.references.remove(key);
-    }
+	public static ReferencesFactory getFactory() {
+
+		if (instanceReferencesFactory == null) {
+			instanceReferencesFactory = new ReferencesFactory();
+		}
+		return instanceReferencesFactory;
+	}
+
+	public int getReference(String key) {
+		Integer result = this.references.get(key);
+		if (result == null) {
+			this.references.put(key, this.reference);
+			result = this.reference;
+			reference++;
+		}
+		return result;
+	}
+
+	public void removeReference(String key) {
+		this.references.remove(key);
+	}
 
 }
--- src/test/java/es/upm/miw/pd/singleton/factory/ReferencesFactoryTest.java
+++ src/test/java/es/upm/miw/pd/singleton/factory/ReferencesFactoryTest.java
@@ -1,5 +1,26 @@
 package es.upm.miw.pd.singleton.factory;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class ReferencesFactoryTest {
 
+    @Test
+    public void testReferencesFactoryIsSingleton() {
+        assertSame(ReferencesFactory.getFactory(), ReferencesFactory.getFactory());
+    }
+
+    @Test
+    public void testReferencesFactorySingletonNotNull() {
+        assertNotNull(ReferencesFactory.getFactory());
+    }
+
+    @Test
+    public void testReferencesFactory() {
+        assertEquals(0, ReferencesFactory.getFactory().getReference("cero"));
+        assertEquals(1, ReferencesFactory.getFactory().getReference("uno"));
+        ReferencesFactory.getFactory().removeReference("cero");
+        assertEquals(2, ReferencesFactory.getFactory().getReference("cero"));
+    }
 }
